0a8a2547e0bbda768231a68a91ba02a29b6c4056
Deskola <denisogunde@gmail.com>
2021-10-25 12:47:18 +0300

    Channel and fields validation

--- lib/helper/env_constants.dart
+++ lib/helper/env_constants.dart
@@ -2,19 +2,18 @@ const String vendorId = "demo";
 const String securityKey = "demoCHANGED";
 const String live = "1";
 const String currency = "KES";
-const String callBackUrl =
-    "https://0f32-178-62-82-156.ngrok.io/test_api/cbk_processor.php";
+const String callBackUrl = "https://example.com";
 const String cst = "1";
 const String crl = "2";
 
 /// Selected channel
 const String mpesa = "1";
-const String bonga = "1";
+const String bonga = "0";
 const String airtel = "1";
 const String equity = "1";
 const String creditcard = "0";
-const String mobilebanking = "1";
-const String mkoporahisi = "1";
+const String mobilebanking = "0";
+const String mkoporahisi = "0";
 const String saida = "0";
 const String elipa = "0";
 const String unionpay = "0";
